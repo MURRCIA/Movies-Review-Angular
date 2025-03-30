e6375b440e17576e0d4f2970056e336d10522244
Murrcia <mzjesusr@gmail.com>
2025-03-30 17:12:27 -0500

    First Clean Commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: false,
+  tmdbApiKey: 'd70ad22b2a6989ca64dbfec3893dbcfd',
+  tmdbAccessToken: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNzBhZDIyYjJhNjk4OWNhNjRkYmZlYzM4OTNkYmNmZCIsIm5iZiI6MTc0MDYzMTYyNy4wMDEsInN1YiI6IjY3YmZlZTRhYzAyOTEzOWIwZTM1OTQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R6hm_hHLA_IM78Hgtibd6yUmIcd9KJDC9kR9Vz4sO_8',
+  apiUrl: 'https://api.themoviedb.org/3'
+};
